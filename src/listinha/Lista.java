afb37d8c4cfa9ffc25c1808b1f93dad4e898a8bf
informatica <informatica@keuan.blumenau.ifc.edu.br>
2017-09-22 09:19:40 -0300

    avançou mais ainda

--- src/listinha/Lista.java
+++ src/listinha/Lista.java
@@ -62,13 +62,99 @@ public class Lista {
         }
     }
 
+    public static void verificaExiste() {
+        boolean existe = false;
+        Pessoa atual;
+        String nome = pedeStr("Informe o nome da pessoa", 1, 100);
+        atual = inicio;
+        while (atual != null) {
+            if (atual.getNome().equals(nome)) {
+                existe = true;
+                break;
+
+            }
+            break;
+        }
+        if (existe) {
+            System.out.println("Nome ENCONTRADO na lista!");
+        } else {
+            System.out.println("Nome NÃO ENCONTRADO na lista");
+        }
+    }
+
+    public static void removeEspecifico() {
+        Pessoa atual;
+        String nome = (pedeStr("Informe o nome a ser excluído", 1, 100));
+        if (nome.equals(inicio.getNome())) {
+            removeInicio();
+        } else if (nome.equals(fim.getNome())) {
+            removeFim();
+        } else {
+            atual = inicio;
+            while (atual != null) {
+                if (atual.getNome().equals(nome)) {
+                    //ligar o anterior ao próximo de atual
+                    Pessoa anterior = buscarAnterior(atual);
+                    anterior.setProximo(atual.getProximo());
+
+                    break;
+                }
+                atual = atual.getProximo();
+            }
+        }
+    }
+
+    public static void removeInicio() {
+        if (qtd == 0) {
+            System.out.println("Lista nula!");
+        } else {
+            inicio = inicio.getProximo();
+        }
+    }
+
+    public static void removeFim() {
+        if (qtd == 0) {
+            System.out.println("Lista nula!");
+        } else {
+            if (inicio == fim) {
+                inicio = null;
+                fim = null;
+            } else {
+                Pessoa atual = inicio;
+                while (atual != null) {
+                    if (atual.getProximo().getProximo() == null) {
+                        atual.setProximo(null);
+                        fim = atual;
+                        break;
+                    }
+                    atual = atual.getProximo();
+                }
+            }
+        }
+    }
+
+    public static Pessoa buscarAnterior(Pessoa p) {
+        Pessoa ret = null;
+        Pessoa atual = inicio;
+        while (atual != null) {
+            if (atual.getProximo() == p) {
+                ret = atual;
+                break;
+            }
+            atual = atual.getProximo();
+        }
+        return ret;
+    }
+
     public static void menu() {
-        System.out.println("-------------------------------------");
+        System.out.println("---------------------------------------------");
         System.out.println("1- Criar lista");
         System.out.println("2- Inserir pessoa no início lista");
         System.out.println("3- Inserir pessoa no final da lista");
         System.out.println("4- Mostrar lista");
-        System.out.println("0- Cancelar");
+        System.out.println("5- Verificar existência de pessoa específica");
+        System.out.println("6- Remover pessoa específica");
+        System.out.println("0- Sair");
     }
 
     public static int pedeInt(String msg, int min, int max) {
@@ -113,7 +199,7 @@ public class Lista {
         int resposta = 0;
         do {
             menu();
-            resposta = pedeInt("Informe a opção desejada:", 0, 5);
+            resposta = pedeInt("Informe a opção desejada:", 0, 7);
             switch (resposta) {
                 case 1:
                     criaLista();
@@ -126,6 +212,12 @@ public class Lista {
                     break;
                 case 4:
                     listaLista();
+                    break;
+                case 5:
+                    verificaExiste();
+                    break;
+                case 6:
+                    removeEspecifico();
                     break;
             }
         } while (resposta != 0);
